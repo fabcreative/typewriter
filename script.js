5995b9086b033ab121f84f73a828d477ebaca407
Fabio Ardemagni <96312583+fabcreative@users.noreply.github.com>
2022-02-15 21:11:07 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+         var typed = new Typed('.input', {
+            strings: ["Video Editor", "Programmer", "Web Developer", "Photo Editor"],
+            typeSpeed: 100,
+            backSpeed:100,
+            loop:true,
+        });
